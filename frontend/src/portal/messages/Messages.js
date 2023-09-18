ed4a15383e6ebf1e2f6b1ab4e8f41d742f097506
AdityaRaut <adityaasr3@gmail.com>
2023-09-18 14:21:11 +0530

    Added the socket.io module to the frontend and the backend.

--- frontend/src/portal/messages/Messages.js
+++ frontend/src/portal/messages/Messages.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
 
 function Messages() {
-  return (
-    <div>messages</div>
-  )
+  useEffect(() => {
+    const socket = io("http://localhost:3000/", {
+      auth: {
+        token: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    socket.on("connect", () => {
+      console.log(socket);
+    });
+    socket.on("message", (msg) => {
+      console.log("server says : ", msg);
+    });
+    socket.emit("message", "hi server", 1, 1);
+  }, []);
+  return <div>messages</div>;
 }
 
-export default Messages
+export default Messages;
